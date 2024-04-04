da4b55511e2cf3d089b5693403fbf7394b89adb2
Mats123 <43936336+Mats123@users.noreply.github.com>
2024-04-04 20:31:09 +0200

    Add files via upload

---
+++ src/button_config.py
@@ -0,0 +1,55 @@
+"""
+This has the same idea as ft_ab_ba_config.py, but with a simple button integration instead of a 
+force-torque sensor.
+"""
+
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Bool
+
+# For safety reason both buttons on a gripper has to be pressed for that gripper to be viewed as 
+# being attached to a pipe.
+def set_config(sensor_list_a, sensor_list_b):
+    total_a = sum(sensor_list_a.values())
+    total_b = sum(sensor_list_b.values())
+
+    if total_a == 2 and total_b == 2:
+        return "both"
+    elif total_a > total_b:
+        return "ab"
+    elif total_a < total_b:
+        return "ba"
+    else:
+        return rospy.logerr(f"Failed to determine configuration. Investigate buttons and code: {e}")
+
+# Each finger is given 1 button. 
+def check_button():
+    sensor_readings_a = {'a00': None, 'a01': None}
+    sensor_readings_b = {'b00': None, 'b01': None}
+
+    def sensor_callback_a(data, sensor_id):
+        nonlocal sensor_readings_a
+        sensor_readings_a[sensor_id] = data.data
+
+    def sensor_callback_b(data, sensor_id):
+        nonlocal sensor_readings_b
+        sensor_readings_b[sensor_id] = data.data
+
+    for sensor_id_a in sensor_readings_a:
+        topic = f"/button_{sensor_id_a}/button"
+        rospy.Subscriber(topic, Bool, sensor_callback_a, sensor_id_a)
+
+    for sensor_id_b in sensor_readings_b:
+        topic = f"/button_{sensor_id_b}/button"
+        rospy.Subscriber(topic, Bool, sensor_callback_b, sensor_id_b)
+    
+    while not all(value is not None for value in sensor_readings_a.values()) or \
+          not all(value is not None for value in sensor_readings_b.values()):
+        rospy.sleep(0.1)
+
+    return set_config(sensor_readings_a, sensor_readings_b)
+
+if __name__ == "__main__":
+    rospy.init_node('button_check_test', anonymous=True)
+    configuration = check_button()
+    print(f"Configuration: {configuration}")
---
+++ src/ft_ab_ba_config.py
@@ -0,0 +1,67 @@
+"""
+This is meant to be a sudo code that could work in theory with integrated force-torque sensors.
+Force-torque sensors have not been integrated on the monkeybot. But this should work as a 
+baseline, if such an aproach for determening the configuration would be taken into consideration.
+This code follows the same structure as prox_ab_ba_config.py file.
+It also uses the same name convention as the monkeybot.
+Note that is necesarely doesnt need to be a force-torque sensor, it could be as simple as buttons
+integrated into the finger of the monkeybot.
+"""
+
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Bool, Float64
+
+# There could be error readings from the force-torque sensor when it isnt touching anything, or some
+# movement when the monkeybot is moving. This supports as a threshold to avoid this potentiol error.
+# Change value as necessary.
+force_threshold = 10.0
+
+def set_config(sensor_list_a, sensor_list_b):
+    total_a = sum(sensor_list_a.values())
+    total_b = sum(sensor_list_b.values())
+
+    if total_a > force_threshold and total_b > force_threshold:
+        return "both"
+    elif total_a > force_threshold:
+        return "ab"
+    elif total_b > force_threshold:
+        return "ba"
+    else:
+        return rospy.logerr(f"Failed to determine configuration. Investigate sensors and 
+                            force threshold: {e}")
+    
+# Each finger is given 1 force-torque sensor. 
+def check_force():
+    sensor_readings_a = {'a00': None, 'a01': None}
+    sensor_readings_b = {'b00': None, 'b01': None}
+
+    def sensor_callback_a(data, sensor_id):
+        nonlocal sensor_readings_a
+        sensor_readings_a[sensor_id] = data.force
+
+    def sensor_callback_b(data, sensor_id):
+        nonlocal sensor_readings_b
+        sensor_readings_b[sensor_id] = data.force
+
+    # Subscribe to a sensor topics
+    for sensor_id_a in sensor_readings_a:
+        topic = f"/force_torque_sensor_{sensor_id_a}/force"
+        rospy.Subscriber(topic, Float64, sensor_callback_a, sensor_id_a)
+
+    # Subscribe to b sensor topics
+    for sensor_id_b in sensor_readings_b:
+        topic = f"/force_torque_sensor_{sensor_id_b}/force"
+        rospy.Subscriber(topic, Float64, sensor_callback_b, sensor_id_b)
+    
+    # Wait for all sensor values to be received
+    while not all(value is not None for value in sensor_readings_a.values()) or \
+          not all(value is not None for value in sensor_readings_b.values()):
+        rospy.sleep(0.1)  # Adjust this as needed
+
+    return set_config(sensor_readings_a, sensor_readings_b)
+
+if __name__ == "__main__":
+    rospy.init_node('force_check_test', anonymous=True)
+    configuration = check_force()
+    print(f"Configuration: {configuration}")
