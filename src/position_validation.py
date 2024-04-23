2570d7f56589370dad547128d2431139fd2dfc4f
Mats123 <43936336+Mats123@users.noreply.github.com>
2024-04-23 11:17:04 +0200

    Update position_validation.py

--- src/position_validation.py
+++ src/position_validation.py
@@ -17,7 +17,7 @@ def main():
 
     M = np.array([[1, 0, 0, 0],
                 [0, -1, 0, 0],
-                [0, 0, -1, -1.4000],
+                [0, 0, -1, -1.4],
                 [0, 0, 0, 1]])
 
     w0a = np.array([-1, 0, 0])
@@ -29,15 +29,14 @@ def main():
     w1b = np.array([0, 1, 0])
     w0b = np.array([-1, 0, 0])
 
-    q0a = np.array([0.1495, 0, -1.4000])
-    q1a = np.array([0, 0, -1.200])
+    q0a = np.array([0.1495, 0, -1.4])
+    q1a = np.array([0, 0, -1.2])
     q2a = np.array([0, 0, -0.9365])
     q3a = np.array([0, 0, -0.8665])
     q3b = np.array([0, 0, -0.5335])
     q2b = np.array([0, 0, -0.4635])
-    q1b = np.array([0, 0, -0.2000])
+    q1b = np.array([0, 0, -0.2])
     q0b = np.array([-0.1495, 0, 0])
-
 
     w = np.array([w0a, w1a, w2a, w3a, w3b, w2b, w1b, w0b])
     q = np.array([q0a, q1a, q2a, q3a, q3b, q2b, q1b, q0b])
