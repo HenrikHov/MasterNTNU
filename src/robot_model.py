87118f2678c0385047bfda048be4e5fdddac4f9a
Mats123 <43936336+Mats123@users.noreply.github.com>
2024-05-05 10:50:15 +0200

    Update robot_model.py

--- src/robot_model.py
+++ src/robot_model.py
@@ -1,7 +1,14 @@
 import numpy  as np
 import modern_robotics as mr
 
+R = np.array([[1, 0, 0],
+              [0, -1, 0],
+              [0, 0, -1]])
 
+M = np.array([[1, 0, 0, 0],
+              [0, -1, 0, 0],
+              [0, 0, -1, -1.400],
+              [0, 0, 0, 1]])
 
 w0a = np.array([-1, 0, 0])
 w1a = np.array([0, -1, 0])
@@ -21,15 +28,29 @@ q2b = np.array([0, 0, -0.4635])
 q1b = np.array([0, 0, -0.200])
 q0b = np.array([-0.1495, 0, 0])
 
+wab = np.array([w0a, w1a, w2a, w3a, w3b, w2b, w1b, w0b])
+qab = np.array([q0a, q1a, q2a, q3a, q3b, q2b, q1b, q0b])
 
-w = np.array([w0a, w1a, w2a, w3a, w3b, w2b, w1b, w0b])
-q = np.array([q0a, q1a, q2a, q3a, q3b, q2b, q1b, q0b])
+vab = np.zeros((8, 3))
+vba = np.zeros((8, 3))
 
-v = np.zeros((8, 3))
+wba = R @ wab.T
+wba = wba.T
 
-for i in range(0, len(w)):
-    v[i] = np.cross(-w[i], q[i])
+one_collum = np.ones((8, 1))
+qab_tilda = np.hstack([qab, one_collum])
+qba = M @ qab_tilda.T
+qba = qba[0:3].T
 
-Blist = np.hstack((w, v)).T
+for i in range(0, len(wab)):
+    vab[i] = np.cross(-wab[i], qab[i])
 
-print(Blist)
+for i in range(0, len(wba)):
+    vba[i] = np.cross(-wba[i], qba[i])
+
+
+Blist_ab = np.hstack((wab, vab)).T
+
+Blist_ba = np.hstack((wba, vba)).T
+
+print(Blist_ba)
