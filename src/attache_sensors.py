e31fdff3f9b948b3fed06bf9f11af4aaa307ae8e
Mats123 <43936336+Mats123@users.noreply.github.com>
2024-05-06 18:34:18 +0200

    Update attache_sensors.py

--- src/attache_sensors.py
+++ src/attache_sensors.py
@@ -1,18 +1,25 @@
 import math
 import numpy as np
+import rospy
 
-def walking(input_length):
-    multiple_sensors = str(input("Attache multiple sensors?[y/n]"))
+def walking_sensor(multiple_sensors):
     if multiple_sensors == "y":
         n_sensors = int(input("Number of sensors:"))
         step_distance = float(input("Distance between sensors"))
         total_length = n_sensors * step_distance
         total_steps = n_sensors
+    elif multiple_sensors == "n":
+        input_length = float(input("Enter length:"))
+        if abs(input_length) > 1.2:
+            step_distance = 1.2
+            total_length = input_length    
+            total_steps = math.ceil(total_length/step_distance)
+        elif abs (input_length) < 0.5:
+            rospy.loginfo("Length input too small, try again with a larger or smaller value than 0.5 or -0.5")
+        else:
+            return input_length
     else:
-        step_distance = 1.2
-        total_length = input_length    
-        total_steps = math.ceil(total_length/step_distance)
-
+        return rospy.loginfo("Input not valid, try again")
     distances = np.zeros(total_steps)
     whole_steps = total_length // step_distance
     remaining_length = total_length % step_distance
@@ -25,4 +32,6 @@ def walking(input_length):
     distances[total_steps] = remaining_length
     return distances
 
-
+def main():
+    multiple_sensors = str(input("Attache multiple sensors?[y/n]"))
+    walking_sensor(multiple_sensors)
